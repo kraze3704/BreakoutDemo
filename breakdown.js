b254be075f3dec10086dd83e33f75c757ab6bbad
changHyunKim <kraze3704@gmail.com>
2019-01-19 00:39:33 +0200

    paddle added, with keyboard input and wall collision

--- breakdown.js
+++ breakdown.js
@@ -6,17 +6,65 @@ const ctx = canvas.getContext("2d");
 const CANVAS_HEIGHT = canvas.height;
 const CANVAS_WIDTH = canvas.width;
 
+
+// ball definition
 const ball = {
     x: canvas.width/2,
     y: canvas.height-30,
 };
-// radius of the ball
 const RADIUS = 10;
 
 // speed of the ball
 let dx = 2;
 let dy = -2;
 
+
+// paddle definition
+const paddleHeight = 10;
+const paddleWidth = 75;
+let paddleX = (CANVAS_WIDTH - paddleWidth)/2;
+const PADDLE_SPEED = 7;
+
+// paddle control variables
+let rightPressed = false;
+let leftPressed = false;
+
+document.addEventListener("keydown", keyDownHandler, false);
+document.addEventListener("keyup", keyUpHandler, false);
+
+
+function movePaddle() {
+    // check for inputs as well as collision with the wall
+    if(rightPressed && paddleX + paddleWidth < CANVAS_WIDTH) {
+        paddleX += PADDLE_SPEED;
+    } else if(leftPressed && paddleX > 0) {
+        paddleX -= PADDLE_SPEED;
+    }
+}
+
+// event handlers for keyboard inputs
+function keyDownHandler(e) {
+    if(e.key == "Right" || e.key == "ArrowRight") {
+        rightPressed = true;
+    } else if(e.key == "left" || e.key == "ArrowLeft") {
+        leftPressed = true;
+    }
+}
+function keyUpHandler(e) {
+    if(e.key == "Right" || e.key == "ArrowRight") {
+        rightPressed = false;
+    } else if(e.key == "left" || e.key == "ArrowLeft") {
+        leftPressed = false;
+    }
+}
+
+function drawPaddle() {
+    ctx.beginPath();
+    ctx.rect(paddleX, CANVAS_HEIGHT - paddleHeight, paddleWidth, paddleHeight);
+    ctx.fillStyle = "#0095DD";
+    ctx.fill();
+    ctx.closePath();
+}
 
 function collisionCheck() {
     // check for top & bottom collision
@@ -39,8 +87,10 @@ function drawBall() {
 
 function draw() {
     window.requestAnimationFrame(draw);
-
     ctx.clearRect(0, 0, CANVAS_WIDTH, CANVAS_HEIGHT);
+
+    movePaddle();
+    drawPaddle();
     drawBall();
     collisionCheck();
     ball.x += dx;
