e5b2d3d203e67c5e69b263d1a50e2adf962d7eca
changHyunKim <kraze3704@gmail.com>
2019-01-19 00:12:45 +0200

    wall collision added

--- breakdown.js
+++ breakdown.js
@@ -3,20 +3,35 @@
 const canvas = document.querySelector("#myCanvas");
 const ctx = canvas.getContext("2d");
 
+const CANVAS_HEIGHT = canvas.height;
+const CANVAS_WIDTH = canvas.width;
+
 const ball = {
     x: canvas.width/2,
     y: canvas.height-30,
 };
+// radius of the ball
+const RADIUS = 10;
 
 // speed of the ball
-const dx = 2;
-const dy = -2;
+let dx = 2;
+let dy = -2;
 
 
+function collisionCheck() {
+    // check for top & bottom collision
+    if (ball.y + dy < RADIUS || ball.y + dy > CANVAS_HEIGHT - RADIUS) {
+        dy = -dy;
+    }
+    // check for left & right collision
+    if (ball.x + dx < RADIUS || ball.x + dx > CANVAS_WIDTH - RADIUS) {
+        dx = -dx;
+    }
+}
 
 function drawBall() {
     ctx.beginPath();
-    ctx.arc(ball.x, ball.y, 10, 0, Math.PI*2);
+    ctx.arc(ball.x, ball.y, RADIUS, 0, Math.PI*2);
     ctx.fillStyle = "#0095DD";
     ctx.fill();
     ctx.closePath();
@@ -25,8 +40,9 @@ function drawBall() {
 function draw() {
     window.requestAnimationFrame(draw);
 
-    ctx.clearRect(0, 0, canvas.width, canvas.height);
+    ctx.clearRect(0, 0, CANVAS_WIDTH, CANVAS_HEIGHT);
     drawBall();
+    collisionCheck();
     ball.x += dx;
     ball.y += dy;
 }
